8a7bdda9faeca339c4d2a72a7dcfeb8aebd7f9f0
duper51 <ianotto24@gmail.com>
2017-12-27 13:38:00 -0700

    ipn changes

--- app.js
+++ app.js
@@ -38,10 +38,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/api', users);
 
-app.use("/ipn", Coinpayments.ipn({
-    merchantId: global.config.coinpayments.merchId,
-    merchantSecret: global.config.coinpayments.merchSecret
-}));
+app.use("/ipn",
+    [Coinpayments.ipn({
+        merchantId: global.config.coinpayments.merchId,
+        merchantSecret: global.config.coinpayments.merchSecret
+    }), function (req, res, next) {
+        res.status(200);
+        res.end();
+    }]);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -53,7 +57,9 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  if(req.app.get('env') === 'development') {
+      console.error(err);
+  }
   // render the error page
   res.status(err.status || 500);
   if(req.url.includes("/api")) {
