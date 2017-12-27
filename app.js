d9cdb30991b51b0c7e29baaca17d6e9112145434
duper51 <ianotto24@gmail.com>
2017-12-27 13:20:55 -0700

    make /ipn IPN URL

--- app.js
+++ app.js
@@ -37,16 +37,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api', users);
+
+app.use("/ipn", Coinpayments.ipn({
+    merchantId: global.config.coinpayments.merchId,
+    merchantSecret: global.config.coinpayments.merchSecret
+}));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-app.use(Coinpayments.ipn({
-    merchantId: global.config.coinpayments.merchId,
-    merchantSecret: global.config.coinpayments.merchSecret
-}));
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
