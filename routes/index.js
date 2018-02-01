2de6a33f979bc8500c5cdee28c5fc4b5205eeea0
duper51 <ianotto24@gmail.com>
2018-02-01 14:01:50 -0700

    error checking to prevent bugs

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,24 @@ const router = express.Router();
 const SocketManager = require('/lib/socket-manager');
 
 router.post('/api/alertbox/execute-test', function(req, res, next) {
-    global.database.pool.query("SELECT id FROM `users` WHERE alertboxApiKey=?", [req.body.alertboxKey], function (err, data) {
-        if(data[0].id === req.body.user_id) {
-            SocketManager.instance.socketManager.to(req.body.alertboxKey).emit({
-                from: "Test Buyer",
-                amount: 0.00245,
-                currency: "TST",
-                message: "This is a test submission of 0.00245 TST",
-                image: "/img/ltc.png"
-            });
-            res.json({success: true});
-        } else {
-            res.json({success: false});
-        }
-    });
+    if(req.body && req.body.alertboxKey && req.body.user_id) {
+        global.database.pool.query("SELECT id FROM `users` WHERE alertboxApiKey=?", [req.body.alertboxKey], function (err, data) {
+            if (data[0].id === req.body.user_id) {
+                SocketManager.instance.socketManager.to(req.body.alertboxKey).emit({
+                    from: "Test Buyer",
+                    amount: 0.00245,
+                    currency: "TST",
+                    message: "This is a test submission of 0.00245 TST",
+                    image: "/img/ltc.png"
+                });
+                res.json({success: true});
+            } else {
+                res.json({success: false});
+            }
+        });
+    } else {
+        res.json({error: "missing parameter"});
+    }
 });
 
 
